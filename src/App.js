2c3b74495e1eef8a9fc7d9c81ee7526d3e4f0f3d
Phat Nguyen <89678375+ngtphat-towa@users.noreply.github.com>
2023-12-17 17:47:30 +0700

    implement onAddTask

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
-import Header from "./components/header";
-import Tasks from "./components/tasks";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState([false]);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -24,15 +26,32 @@ const App = () => {
     },
   ]);
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+  // Delete Task
   const deleteTask = (id) => {
-    console.log("delete", id);
     setTasks(tasks.filter((task) => task.id !== id));
   };
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
       <Header></Header>
+      <AddTask onAdd={addTask}></AddTask>
       {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} />
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
         "No task to shows"
       )}
--- src/components/tasks.js
+++ src/components/tasks.js
@@ -1,12 +1,16 @@
 import React from "react";
-import Task from "./task";
+import Task from "./Task";
 
-
-const Tasks = ({tasks, onDelete}) => {
+const Tasks = ({ tasks, onDelete, onToggle }) => {
   return (
     <>
       {tasks.map((task, index) => (
-        <Task key={index} task={task} onDelete={onDelete}></Task>
+        <Task
+          key={index}
+          task={task}
+          onDelete={onDelete}
+          onToggle={onToggle}
+        ></Task>
       ))}
     </>
   );
